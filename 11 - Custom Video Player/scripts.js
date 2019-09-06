942703195ecf5a4525b8e2031b77ae0e1d70ae9f
Simon Pioli <simon@simonpioli.com>
2019-09-06 16:10:10 +0100

    Day 11 additional work - implement full screen button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,14 +6,16 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
-let mousedown = false;
+const fullscreenButton = player.querySelector('.fullscreen');
 
 // Functions
 function togglePlay() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
+    if (!isFullscreen) { // Interferes with native controls in fullscreen
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
     }
 }
 
@@ -38,16 +40,32 @@ function scrub(e) {
     video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+    video.requestFullscreen();
+}
+
 // Events
+
+// playback / skip time buttons
+let isFullscreen = false;
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+// volume / playback speed sliders
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+// progress / scrubbing
+let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+// fullscreen
+fullscreenButton.addEventListener('click', toggleFullscreen);
+video.addEventListener('fullscreenchange', () => isFullscreen = !isFullscreen);
